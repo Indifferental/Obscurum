d5b5923af6a902bde85353ff1251cd01b606fa78
Indifferental <70567512+Indifferental@users.noreply.github.com>
2025-01-25 03:25:38 +0700

    Update user.js

--- scripts/user.js
+++ scripts/user.js
@@ -7,6 +7,7 @@
 // @match        https://*.tankionline.com/*
 // @icon         https://github.com/Indifferental/Obscurum/blob/main/assets/icons/flame90.png?raw=true
 // @grant        GM_xmlhttpRequest
+// @grant        unsafeWindow
 // @run-at       document-start
 // ==/UserScript==
 
