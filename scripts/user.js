8f64c1b9de2d82d3d6f9d1d3254c19ef2a4d2b28
Indifferental <70567512+Indifferental@users.noreply.github.com>
2025-01-25 02:57:26 +0700

    Update user.js

--- scripts/user.js
+++ scripts/user.js
@@ -2,12 +2,11 @@
 // @name         Obscurum
 // @namespace    http://tampermonkey.net/
 // @version      none
-// @description  none
+// @description  An open-source theme for Tanki Online
 // @author       Indifferental
 // @match        https://*.tankionline.com/*
 // @icon         https://github.com/Indifferental/Obscurum/blob/main/assets/icons/flame90.png?raw=true
 // @grant        GM_xmlhttpRequest
-// @grant        unsafeWindow
 // @run-at       document-start
 // ==/UserScript==
 
